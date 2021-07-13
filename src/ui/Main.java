957c96b92a326a879a9165ff884a499ce74d3342
Camilo Pereyra <63877199+CamiloPereyra@users.noreply.github.com>
2021-07-13 18:47:41 -0300

    casi menu

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,6 +5,19 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		Menu m=new Menu();
-		//m.start();
+		m.start();
+		/*try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		*/
+		
+		//Metodos..
+		
+		
+		
+		System.out.println("\n -- FIN DEL PROGRAMA -- ");
 	}
 }
--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,6 +1,96 @@
 package ui;
 
+import java.util.Scanner;
 
 public class Menu {
+
+	Scanner scan = new Scanner(System.in);
+	public void start() {
+		
+		String rta = null;
+		System.out.print(" \n-- COMANDOS --");	
+		System.out.print("\n abrir bitacora");
+		System.out.print("\n cargaIntegrantes");
+		System.out.print("\n cargaArmas");
+		System.out.print("\n cargaRoles");
+		System.out.print("\n cargaRobos");
+		System.out.print("\n cargaMedallas");
+		System.out.print("\n cargaRangos");
+		System.out.print("\n cargaSubdivisiones");
+		System.out.print("\n cargaRangoSub");
+		System.out.print("\n stockArmas");
+		System.out.print("\n registrarUnRol");
+		System.out.print("\n registrarUnRango");
+		System.out.print("\n registrarHorasJugadas");
+		System.out.print("\n registrarUnRobo");
+		System.out.print("\n registrarUnaMedalla");
+		System.out.print("\n registrarUnRangoSub");
+		System.out.print("\n salir");
+		do {
+			rta = inputCommand();
+			switch(rta) {
+			case "abrir bitacora":
+				break;
+			case "cargaIntegrantes":
+				break;
+			case "cargaArmas":
+				break;
+				
+			case "cargaRoles":
+				break;
+			
+			case "cargaRobos":
+				break;
+			
+			case "cargaMedallas":
+				break;
+			
+			case "cargaRangos":
+				break;
+			case "cargaSubdivisiones":
+				break;
+				
+			case "cargaRangoSub":
+				break;
+			case "stockArmas":
+				break;
+			case "registrarUnRol":
+				break;
+			case "registrarUnRango":
+				break;
+			case "registrarHorasJugadas":
+				break;
+			case "registrarUnRobo":
+				break;
+			case "registrarUnaMedalla":
+				break;
+			case "registrarUnRangoSub":
+				break;
+							
+			case "salir":
+				close();
+				break;
+			default:
+				System.out.print(" Comando invalido");
+				break;
+			
+			}
+		
+		
+		}while(!rta.equals("salir"));
+		
+		
+	}
+	
+	private String inputCommand() {
+		
+		
+		System.out.print("\n Escriba un comando:");
+		return scan.nextLine();
+	}
+	
+	private void close() {
+		scan.close();
+	}
 
 }
