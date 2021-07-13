8cbc4731c84f57e25ffa564d6a627207383da4eb
Luciano Ragusa <80652919+Luchoragusa@users.noreply.github.com>
2021-07-13 20:42:40 -0300

    menu

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -39,7 +39,7 @@ public class Menu
 			}
 				break;
 			case "cargaIntegrantes":{
-				cargaIntegrante();
+				newIntegrante();
 			}
 				break;
 			case "cargaArmas":{
@@ -70,6 +70,72 @@ public class Menu
 				
 			}
 				break;
+			case "actualizarIntegrantes":{
+				updateIntegrante();
+			}
+				break;
+			case "actualizarArmas":{
+				
+			}
+				break;
+			case "actualizarRoles":{
+				
+			}
+				break;
+			case "actualizarRobos":{
+				
+			}
+				break;
+			case "actualizarMedallas":{
+				
+			}
+				break;
+			case "actualizarRangos":{
+				
+			}
+				break;
+			case "actualizarSubdivisiones":{
+				
+			}
+				break;
+			case "actualizarRangoSub":{
+				
+			}
+				break;
+			
+			case "mostrarIntegrantes":{
+				
+			}
+				break;
+			case "mostrarArmas":{
+				
+			}
+				break;
+			case "mostrarRoles":{
+				
+			}
+				break;
+			case "mostrarRobos":{
+				
+			}
+				break;
+			case "mostrarMedallas":{
+				
+			}
+				break;
+			case "mostrarRangos":{
+				
+			}
+				break;
+			case "mostrarSubdivisiones":{
+				
+			}
+				break;
+			case "mostrarRangoSub":{
+				
+			}
+				break;
+				
 			case "registrarUnStock":{
 				
 			}
@@ -125,6 +191,24 @@ public class Menu
 		System.out.print("\n cargaSubdivisiones");
 		System.out.print("\n cargaRangoSub");
 		
+		System.out.print("\n actualizarIntegrantes");
+		System.out.print("\n actualizarArmas");
+		System.out.print("\n actualizarRoles");
+		System.out.print("\n actualizarRobos");
+		System.out.print("\n actualizarMedallas");
+		System.out.print("\n actualizarRangos");
+		System.out.print("\n actualizarSubdivisiones");
+		System.out.print("\n actualizarRangoSub");
+		
+		System.out.print("\n mostrarIntegrantes");
+		System.out.print("\n mostrarArmas");
+		System.out.print("\n mostrarRoles");
+		System.out.print("\n mostrarRobos");
+		System.out.print("\n mostrarMedallas");
+		System.out.print("\n mostrarRangos");
+		System.out.print("\n mostrarSubdivisiones");
+		System.out.print("\n mostrarRangoSub");
+		
 		System.out.print("\n registrarUnStock");
 		System.out.print("\n registrarUnRol");
 		System.out.print("\n registrarUnRango");
@@ -147,7 +231,33 @@ public class Menu
 		s.close();
 	}
 	
-	private void cargaIntegrante()
+	public Integrante login() 
+	{
+		Integrante i=new Integrante();
+		
+		System.out.print("Email: ");
+		i.setUsuario(s.nextLine());
+
+		System.out.print("Password: ");
+		i.setPw(s.nextLine());
+		
+		i=ctrlLogin.validate(i);
+		
+		return i;
+	}
+	
+	private Integrante find() 
+	{
+		System.out.println();
+		Integrante i=new Integrante();
+		
+		System.out.print("Ingrese una id: ");
+		i.setIdIntegrante(Integer.parseInt(s.nextLine()));
+		
+		return ctrlLogin.getByIdIntegrante(i);
+	}
+	
+	private void newIntegrante()
 	{
 		System.out.println();
 		Integrante i=new Integrante();
@@ -172,6 +282,63 @@ public class Menu
 		ctrlLogin.add(i); // agrego la persona
 		dr.saveRole(i,r); // agrego el rol a la tabla de rol_persona
 		
+		System.out.println("Operacion realizada con Exito");
+	}
+	
+	private void updateIntegrante() 
+	{
+		System.out.println();
+		Integrante i=new Integrante();
+
+		Rol r = new Rol(); i.addRol(r);
+		DataRol dr = new DataRol();
+		
+		i = find();
+
+		System.out.print("Apellido(ACTUAL): " + i.getApellido());
+		System.out.println();
+		System.out.print("Apellido(NUEVO): "); i.setApellido(s.nextLine());
+		
+		System.out.print("Nombre(ACTUAL): " + i.getNombre());
+		System.out.println();
+		System.out.print("Nombre(NUEVO): "); i.setNombre(s.nextLine());
+
+		System.out.print("Usuario(ACTUAL): " + i.getUsuario());
+		System.out.println();
+		System.out.print("Usuario(NUEVO): "); i.setUsuario(s.nextLine());
+		
+		System.out.print("Usuario(ACTUAL): " + i.getUsuario());
+		System.out.println();
+		System.out.print("Usuario(NUEVO): "); i.setUsuario(s.nextLine());
+		
+		System.out.print("SteamHex(ACTUAL): " + i.getSteamHex());
+		System.out.println();
+		System.out.print("SteamHex(NUEVO): "); i.setSteamHex(s.nextLine());
+		
+		System.out.print("DiscordId(ACTUAL): " + i.getDiscordId());
+		System.out.println();
+		System.out.print("DiscordId(NUEVO): "); i.setDiscordId(s.nextLine());
+		
+		System.out.print("PW(NUEVO): "); i.setPw(s.nextLine());
+		System.out.println();
+		
+		dr.deleteRole(i);
+		
+		System.out.println("Lista de roles ");
+		System.out.println(dr.getAll());
+		
+		System.out.print("Ingrese la descripcion del rol: "); 
+		r.setDescripcion(s.nextLine());
+		
+		r = dr.getByDesc(r);
+		
+		System.out.println(r.toString());
+		
+		ctrlLogin.update(i); // agrego la persona
+		
+		dr.saveRole(i,r); // agrego el rol a la tabla de rol_persona
+		
+		System.out.println();
 		System.out.println("Operacion realizada con Exito");
 	}
 
