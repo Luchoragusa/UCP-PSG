4f4b7e3e677d03812eaf86c6f90f321f89269ecc
JuaniPucheta <nachop2000@gmail.com>
2021-07-23 16:35:01 -0300

    cambios

--- src/data/DataMed_Integrante.java
+++ src/data/DataMed_Integrante.java
@@ -81,7 +81,7 @@ public class DataMed_Integrante {
 		
 		PreparedStatement stmt=null;
 		ResultSet rs=null;
-		try 
+		try  
 		{
 			stmt=DbConnector.getInstancia().getConn().prepareStatement(
 					  "select medalla.* "
